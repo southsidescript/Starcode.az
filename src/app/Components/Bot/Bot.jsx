9234411dd15fc9d8cd9d1a1f03bc8866dde5903f
Serkhan <106021920+southsidescript@users.noreply.github.com>
2025-01-06 19:29:10 +0300

    'addnewbotfunc'

--- src/app/Components/Bot/Bot.jsx
+++ src/app/Components/Bot/Bot.jsx
@@ -1,11 +1,47 @@
+'use client'
+
+import { useState } from 'react';
 import './Bot.scss'
 
 const Bot = () => {
+     
+    const [bot,setBot] = useState({
+        name:'sparky',
+        token:'8158782146:AAFAbhVHbQ_Ot3n-zgOd_jgcJ_vlliEZ_FY',
+        users:{
+            serkhan:688804750,
+            bahadir:535716676
+        },
+        request_url:'https://api.telegram.org/bot',
+        commands:{
+            msg:'sendMessage?chat_id=<chat_id>&text=<text>'
+        }
+    })
+    const [clientInfo,setClientInfo] = useState('');
+
+
+    const sendRequest = () => {
+        
+        const url = bot.request_url + bot.token + '/'+ bot.commands.msg;
+        const chat_id = bot.users.serkhan;
+        const text = 'Клиент начал заполнение формы!';
+        const request_url = url.replace('<chat_id>',chat_id).replace('<text>',text);
+        
+        fetch(request_url)
+        .then(res  => res.json())
+        .then(data =>{ console.log(data)
+            setClientInfo(data)
+        })
+         
+
+
+    }
+
     return (
 
 
         
-        <div className='bot'>
+        <div className='bot'  onClick={sendRequest}>
            <img src="./bot/bot-v1.png" alt="" />
         </div>
     )
