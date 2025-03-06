abfcaaafead80266dbaa507fe4aba74702ccd5ac
Serkhan <106021920+southsidescript@users.noreply.github.com>
2025-03-06 06:07:20 -0500

    u

--- src/app/Components/pages/Footer/Footer.jsx
+++ src/app/Components/pages/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import style from '@/styles/global.module.scss'
 import GoogleMap from '../../organisms/GoogleMaps/GoogleMap';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer =  () => {
  
@@ -30,12 +31,28 @@ const Footer =  () => {
          <div className={style.starcode_footer_info}>
 
           <div className={style.starcode_footer_info_title} >
-            <Image src={'/icons/starcode.jpg'} alt={'starcode'} width={40} height={40}/>
-             <div>
-             <h2>Starcode</h2>
-        
+            <Image src={'/icons/starcode.png'} alt={'starcode'} width={40} height={40}/>
+              <div>
+              <h2>Starcode</h2>
              </div>
      
+          </div>
+
+          <div className={style.starcode_footer_info_links}>
+                   <Link href={''}>  <p>Modullar</p> </Link>
+                   <Link href={''}>  <p>ətraflı</p> </Link>
+                   <Link href={''}>  <p>texnologiyalar</p> </Link>
+                   <Link href={''}>  <p>əlaqə saxla</p> </Link>
+          </div>
+
+          <div className={style.starcode_footer_info_contacts}>
+            <Link href={'mailto:info@starcode.az'}> <p>info@starcode.az</p> </Link>
+            <p>+994 50 832 22 44</p>
+          </div>
+
+          <div className={style.starcode_footer_info_medias}>
+             <Link href={'https://www.instagram.com/starcode.az/'}> <Image src={'/icons/in.png'} alt={'instagram'} width={25} height={25}/> </Link>
+             <Link target='_blank' href={'https://wa.me/+994508322244'}> <Image src={'/icons/wa.png'} alt={'whatsapp'} width={25} height={25}/> </Link>
           </div>
          
 
